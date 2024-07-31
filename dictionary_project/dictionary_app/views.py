1ffcd66a9c9c5853c1bec904b03b75b9eb394517
ShreyaC8 <shreya.chawla218@outlook.com>
2024-07-31 19:44:20 +0100

    Update views.py

--- dictionary_project/dictionary_app/views.py
+++ dictionary_project/dictionary_app/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-from .scholarships import generic
-from .scholarships import agri_vet
+from .scholarships import schol_type
 
 def search_dictionary(request):
     query = request.GET.get('query', '')
     results = {}
     
     if query:
-        results = {k: v for k, v in generic.items() or v in agri_vet.items() if query.lower() in k.lower()}
+        results = {k: v for k, v in schol_type.items() if query.lower() in k.lower()}
 
     return render(request, 'dictionary_app/search.html', {'results': results, 'query': query})
